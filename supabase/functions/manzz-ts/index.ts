e98190c2394b0cbcbdabeb4e4998cf734b20b178
m2lgl <208121602+m2lgl@users.noreply.github.com>
2026-03-26 21:21:39 +0700

    update supabase/functions/manzz-ts/index.ts

--- supabase/functions/manzz-ts/index.ts
+++ supabase/functions/manzz-ts/index.ts
@@ -1,7 +1,7 @@
 import { Hono } from 'npm:hono'
 
 const app = new Hono()
-
+app.basePath("/manzz-ts")
 // 1. Definisikan Interface buat TypeScript (Biar Gak Merah-merah)
 interface TelegramUpdate {
   message?: {
