9d688c6adf07f2d9e0be1f3717d4176aa47fe072
m2lgl <208121602+m2lgl@users.noreply.github.com>
2026-03-26 21:55:13 +0700

    update supabase/functions/manzz-ts/index.ts

--- supabase/functions/manzz-ts/index.ts
+++ supabase/functions/manzz-ts/index.ts
@@ -3,7 +3,7 @@ import { Hono } from 'npm:hono'
 const app = new Hono()
 
 // JURUS SAPU JAGAT: Terima semua (GET/POST) di path mana pun
-app.all('*', async (c) => {
+app.get('/function/v1/manzz-ts', async (c) => {
   const path = c.req.path
   console.log(`[LOG] Ada tamu ngetok di path: ${path}`)
 
